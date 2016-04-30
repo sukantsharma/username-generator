4b1b1cc4dd64e8351a2a137db8bc714283235dd7
sukantsharma <sukant929@protonmail.com>
2016-04-30 23:02:41 +0530

    Added files via upload

---
+++ username.py
@@ -0,0 +1,86 @@
+print("\nusername generator by Sukant | sukant929@protonmail.com\n")
+f_name = raw_input("Enter First Name: ")
+l_name = raw_input("Enter Last Name: ")
+num = raw_input("Enter Birth Year: ")
+print("\n---------------------------------------\n")
+print(f_name) #sukant
+print(l_name) #sharma
+print(f_name + l_name) #sukantsharma
+print(l_name + f_name) #sharmasukant
+print(f_name + "." + l_name) #sukant.sharma
+print(l_name + "." + f_name) #sharma.sukant
+print(f_name + "_" + l_name) #sukant_sharma
+print(l_name + "_" + f_name) #sharma_sukant
+print(f_name + num) #sukant123
+print(num + f_name) #123sukant
+print(l_name + num) #sharma123
+print(num + l_name) #123sharma
+print(f_name + l_name + num) #sukantsharma123
+print(f_name + num + l_name) #sukant123sharma
+print(num + f_name + l_name) #123sukantsharma
+print(l_name + f_name + num) #sharmasukant123
+print(l_name + num + f_name) #sharma123sukant
+print(num + l_name + f_name) #123sharmasukant
+print(f_name + "." + l_name + num) #sukant.sharma123
+print(f_name + "." + num + l_name) #sukant.123sharma
+print(f_name + num + "." + l_name) #sukant123.sharma
+print(num + f_name + "." + l_name) #123sukant.sharma
+print(l_name + "." + f_name + num) #sharma.sukant123
+print(l_name + "." + num + f_name) #sharma.123sukant
+print(l_name + num + "." + f_name) #sharma123.sukant
+print(num + l_name + "." + f_name) #123sharma.sukant
+print(f_name + "_" + l_name + num) #sukant_sharma123
+print(f_name + "_" + num + l_name) #sukant_123sharma
+print(f_name + num + "_" + l_name) #sukant123_sharma
+print(num + f_name + "_" + l_name) #123sukant_sharma
+print(l_name + "_" + f_name + num) #sharma_sukant123
+print(l_name + "_" + num + f_name) #sharma_123sukant
+print(l_name + num + "_" + f_name) #sharma123_sukant
+print(num + l_name + "_" + f_name) #123sharma_sukant
+print(f_name + l_name[0]) #sukants
+print(l_name + f_name[0]) #sharmas
+print(f_name[0] + l_name) #ssharma
+print(l_name[0] + f_name) #ssukant
+print(f_name[0] + "." + l_name) #s.sharma
+print(l_name[0] + "." + f_name) #s.sukant
+print(f_name[0] + "_" + l_name) #s_sharma
+print(l_name[0] + "_" + f_name) #s_sukant
+print(f_name + "." + l_name[0]) #sukant.s
+print(l_name + "_" + f_name[0]) #sharma_s
+print(f_name + l_name[0] + num) #sukants123
+print(f_name + num + l_name[0]) #sukant123s
+print(num + f_name + l_name[0]) #123sukants
+print(l_name + f_name[0] + num) #sharmas123
+print(l_name + num + f_name[0]) #sharma123s
+print(num + l_name + f_name[0]) #123sharmas
+print(f_name[0] + l_name + num) #ssharma123
+print(f_name[0] + num + l_name) #s123sharma
+print(num + f_name[0] + l_name) #123ssharma
+print(l_name[0] + f_name + num) #ssukant123
+print(l_name[0] + num + f_name) #s123sukant
+print(num + l_name[0] + f_name) #123ssukant
+print(f_name[0] + "." + l_name + num) #s.sharma123
+print(f_name[0] + "." + num + l_name) #s.123sharma
+print(f_name[0] + num + "." + l_name) #s123.sharma
+print(num + f_name[0] + "." + l_name) #123s.sharma
+print(l_name[0] + "." + f_name + num) #s.sukant123
+print(l_name[0] + "." + num + f_name) #s.123sukant
+print(l_name[0] + num + "." + f_name) #s123.sukant
+print(num + l_name[0] + "." + f_name) #123s.sukant
+print(f_name[0] + "_" + l_name + num) #s_sharma123
+print(f_name[0] + "_" + num + l_name) #s_123sharma
+print(f_name[0] + num + "_" + l_name) #s123_sharma
+print(num + f_name[0] + "_" + l_name) #123s_sharma
+print(l_name[0] + "_" + f_name + num) #s_sukant123
+print(l_name[0] + "_" + num + f_name) #s_123sukant
+print(l_name[0] + num + "_" + f_name) #s123_sukant
+print(num + l_name[0] + "_" + f_name) #123s_sukant
+print(f_name + "." + l_name[0] + num) #sukant.s123
+print(f_name + "." + num + l_name[0]) #sukant.123s
+print(f_name + num + "." + l_name[0]) #sukant123.s
+print(num + f_name + "." + l_name[0]) #123sukant.s
+print(l_name + "_" + f_name[0] + num) #sharma_s123
+print(l_name + "_" + num + f_name[0]) #sharma_123s
+print(l_name + num + "_" + f_name[0]) #sharma123_s
+print(num + l_name + "_" + f_name[0]) #123sharma_s
+print("\n---------------------------------------\n")
